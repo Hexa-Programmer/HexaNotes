cabaf77d00f4c599f95c8be593ec9a456ea478bb
Hexa-Programmer <harsukh669@gmail.com>
2026-06-22 11:25:55 +0530

    Changed the WebApp from "HexaTools" to "HexaNotes"

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
 // script.js
 
 //Hidden Pages:
-document.getElementById("notes").style.display = "none";
 
 //Variables:
 const notes_btn = document.getElementById("notes_btn");
 const noteBox = document.getElementById("noteBox");
 const noteTitle = document.getElementById("noteTitle")
 
-//Notes:
-notes_btn.addEventListener("click", function () {
-  document.getElementById("dashboard").style.display = "none";
-  document.getElementById("notes").style.display = "block";
-});
+
 
 noteBox.value = localStorage.getItem("hexa_notes") || "";
 
