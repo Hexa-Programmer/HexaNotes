287d9b3b8f8541ef5513c6ed967f0768f2dd57b5
Hexa-Programmer <harsukh669@gmail.com>
2026-06-22 10:43:15 +0530

    Added the tool "Notes"

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // script.js
+
+//Hidden Pages:
+document.getElementById("notes").style.display = "none";
+
+//Variables:
+const notes_btn = document.getElementById("notes_btn");
+const noteBox = document.getElementById("noteBox");
+const noteTitle = document.getElementById("noteTitle")
+
+//Notes:
+notes_btn.addEventListener("click", function () {
+  document.getElementById("dashboard").style.display = "none";
+  document.getElementById("notes").style.display = "block";
+});
+
+noteBox.value = localStorage.getItem("hexa_notes") || "";
+
+noteBox.addEventListener("input", function () {
+    localStorage.setItem("hexa_notes", noteBox.value);
+});
+
+noteTitle.value = localStorage.getItem("hexa_note_title") || "";
+
+noteTitle.addEventListener("input", function () {
+    localStorage.setItem("hexa_note_title", noteTitle.value);
+});
